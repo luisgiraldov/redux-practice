d9edcff9f3854f3da3a790d4219d5c32613a17b0
Luis Giraldo <luisgiraldo@Luiss-MacBook-Pro.local>
2021-04-17 19:44:09 -0400

    Added DashboardPage and PostsPage components and added routes to display these pages (components)

--- src/App.js
+++ src/App.js
@@ -1,7 +1,19 @@
 import React from 'react';
+import { BrowserRouter as Router, Switch, Route, Redirect } from 'react-router-dom';
+
+import DashboardPage from './pages/DashboardPage.component';
+import PostsPage from './pages/PostsPage.component';
 
 const App = () => {
-    return <div>Hello, Redux</div>
+    return (
+        <Router>
+            <Switch>
+                <Route exact path="/" component={DashboardPage} />
+                <Route exact path="/posts" component={PostsPage} />
+                <Redirect to="/" />
+            </Switch>
+        </Router>
+    );
 }
 
 export default App;
