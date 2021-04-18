bd66d1950b2ab93941b9718db8d855e7502436c7
Luis Giraldo <luisgiraldo@Luiss-MacBook-Pro.local>
2021-04-17 20:38:19 -0400

    Added Post component to PostsPage to display state on the page

--- src/pages/PostsPage.component.jsx
+++ src/pages/PostsPage.component.jsx
@@ -4,15 +4,26 @@ import { connect } from 'react-redux';
 //Bring in the asynchronous fetchPosts action
 import { fetchPosts } from '../actions/postsActions';
 
+//import Post to display post info
+import { Post } from '../components/Post.component';
+
 //Redux state is now in the props of the component
 const PostsPage = ({ dispatch, loading, posts, hasErrors }) => {
     useEffect(() => {
         dispatch(fetchPosts())
-    }, [dispatch])
+    }, [dispatch]);
+
+    //Show loading, error, or success state
+    const renderPosts = () => {
+        if(loading) return <p>Loading posts...</p>
+        if(hasErrors) return <p>Unable to display posts.</p>
+        return posts.map((post) => <Post key={post.id} post={post} />)
+    }
 
     return (
         <section>
             <h1>Posts</h1>
+            {renderPosts()}
         </section>
     )
 }
