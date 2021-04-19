024bf43174fb2ba42bbeb813f5054bb35bc5ceee
Luis Giraldo <luisgiraldo@Luiss-MacBook-Pro.local>
2021-04-18 22:30:30 -0400

    Added Navbar component

--- src/App.js
+++ src/App.js
@@ -1,6 +1,7 @@
 import React from 'react';
 import { BrowserRouter as Router, Switch, Route, Redirect } from 'react-router-dom';
 
+import { Navbar } from './components/Navbar.component';
 import DashboardPage from './pages/DashboardPage.component';
 import PostsPage from './pages/PostsPage.component';
 import SinglePostPage from './pages/SinglePostPage.component';
@@ -8,6 +9,7 @@ import SinglePostPage from './pages/SinglePostPage.component';
 const App = () => {
     return (
         <Router>
+            <Navbar />
             <Switch>
                 <Route exact path="/" component={DashboardPage} />
                 <Route exact path="/posts" component={PostsPage} />
