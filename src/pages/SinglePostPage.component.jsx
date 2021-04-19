01f78537c5bbbbbac74186c5b01dacb15cc9e227
Luis Giraldo <luisgiraldo@Luiss-MacBook-Pro.local>
2021-04-18 22:46:53 -0400

    Added comments Component with its actions and reducers

--- src/pages/SinglePostPage.component.jsx
+++ src/pages/SinglePostPage.component.jsx
@@ -2,8 +2,10 @@ import React, { useEffect } from 'react';
 import { connect } from 'react-redux';
 
 import { fetchPost } from '../actions/postActions';
+import { fetchComments } from '../actions/commentsActions';
 
 import { Post } from '../components/Post.component';
+import { Comment } from '../components/Comment.component';
 
 const SinglePostPage = ({
     match,
@@ -11,10 +13,12 @@ const SinglePostPage = ({
     post,
     hasErrors,
     loading,
+    comments
 }) => {
     useEffect(() => {
         const { id } = match.params;
 
+        dispatch(fetchComments(id))
         dispatch(fetchPost(id))
     },[dispatch, match]);
 
@@ -25,20 +29,34 @@ const SinglePostPage = ({
         return <Post post={post} />
     }
 
+    const renderComments = () => {
+        if (loading.comments) return <p>Loading comments...</p>
+        if (hasErrors.comments) return <p>Unable to display comments.</p>
+    
+        return comments.map(comment => (
+          <Comment key={comment.id} comment={comment} />
+        ))
+    }
+
     return (
         <section>
             {renderPost()}
+            <h2>Comments</h2>
+            {renderComments()}
         </section>
     );
 };
 
 const mapStateToProps = state => ({
     post: state.post.post,
+    comments: state.comments.comments,
     loading: {
         post: state.post.loading,
+        comments: state.comments.loading
     },
     hasErrors: {
         post: state.post.hasErrors,
+        comments: state.comments.hasErrors
     }
 });
 
